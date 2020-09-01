659f9caf8d09434283a2e36ef684ce6b328bfea7
Hugo Lefrancq <hugo.lefrancq@gmail.com>
2020-09-01 12:36:55 +0200

    Display match ids, add delete command to delete a match

--- rl-match-tracker.js
+++ rl-match-tracker.js
@@ -18,7 +18,13 @@ const args = argv.option([
     name: 'comment',
     short: 'c',
     type: 'string'
-  }, {
+  },
+  {
+    name: 'delete',
+    short: 'd',
+    type: 'int'
+  },
+  {
     name: 'help',
     short: 'h',
     type: 'boolean'
@@ -34,7 +40,11 @@ if (args.options.help) {
   console.log('    example: -j \'Sabodji\' \'Fleeqq\'\n')
   console.log(clc.bold('-c') + ' or ' + clc.bold('--comment') + '  =>  Add a comment related to the match (Like the opponent team name, their MMR average or the match type...')
   console.log('    example: -c \'Scrim 1700/1800\'\n')
-  console.log(clc.bold.cyan('Full command example => ') + 'node rl-match-tracker -s 4,2 -j \'Sabodji\' \'Fleeqq\' -c \'Scrim 1700/1800\'')
+  console.log(clc.bold('-d') + ' or ' + clc.bold('--delete') + '  =>  Delete a match (with a specific id)')
+  console.log('    example: -d 3\n')
+  console.log(clc.bold.cyan('Full list match command example => ') + 'node rl-match-tracker')
+  console.log(clc.bold.cyan('Full add match command example => ') + 'node rl-match-tracker -s 4,2 -j \'Sabodji\' \'Fleeqq\' -c \'Scrim 1700/1800\'')
+  console.log(clc.bold.cyan('Full delete match command example => ') + 'node rl-match-tracker -d 2')
   process.exit(1)
 }
 
@@ -50,6 +60,17 @@ const db = new sqlite3.Database(path.join(__dirname, 'tracker.db'))
 db.run(initSql, err => {
   if (err) {
     throw err
+  }
+
+  // Delete match
+  if (args.options.delete) {
+    const deleteSql = `DELETE FROM match WHERE match_id = ?`
+    const params = [args.options.delete]
+    db.run(deleteSql, params, err => {
+      if (err) {
+        throw err
+      }
+    })
   }
 
   // Insert match
@@ -79,9 +100,9 @@ db.run(initSql, err => {
     }
     rows.forEach(match => {
       if (match.team_score > match.opponent_team_score) {
-        console.log(clc.green(`${emoji.get('trophy')} ${match.team_score} - ${match.opponent_team_score} `) + `${match.players ? emoji.get('two_men_holding_hands') + ' ' + match.players : ''} ${match.comment ? emoji.get('speech_balloon') + ' ' + match.comment : ''}`)
+        console.log(clc.green(`[${match.match_id}] ${emoji.get('trophy')} ${match.team_score} - ${match.opponent_team_score} `) + `${match.players ? emoji.get('two_men_holding_hands') + ' ' + match.players : ''} ${match.comment ? emoji.get('speech_balloon') + ' ' + match.comment : ''}`)
       } else {
-        console.log(clc.red(`${emoji.get('disappointed_relieved')} ${match.team_score} - ${match.opponent_team_score} `) + `${match.players ? emoji.get('two_men_holding_hands') + ' ' + match.players : ''} ${match.comment ? emoji.get('speech_balloon') + ' ' + match.comment : ''}`)
+        console.log(clc.red(`[${match.match_id}] ${emoji.get('disappointed_relieved')} ${match.team_score} - ${match.opponent_team_score} `) + `${match.players ? emoji.get('two_men_holding_hands') + ' ' + match.players : ''} ${match.comment ? emoji.get('speech_balloon') + ' ' + match.comment : ''}`)
       }
     })
   })
